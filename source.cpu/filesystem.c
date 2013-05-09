aab3b69770f0c218d8ddb9f88b1efc079f3e00f5
Jared Nelson <jared.er2000jared@gmail.com>
2013-05-09 04:48:40 -0700

    delete now working, change inode structure to include 7 character string for filename, created interface, have not throughly tested it yet though.

--- source.cpu/filesystem.c
+++ source.cpu/filesystem.c
@@ -11,6 +11,10 @@ int end;//tracks end of meta data
 int waste;//tracks wasted space, ie space that is too small for header data, this will be useful for determining when to defragment
 
 /**
+ * Search for IFACE to find interface items
+ */
+
+/**
  * Description of structure for record
  * +-----------------------------------------------------------------------------------+
  * |prev 0 if none (1)|next 0 if none(1)|file id(1)|size of data(1)|data(?)|next record|
@@ -19,22 +23,23 @@ int waste;//tracks wasted space, ie space that is too small for header data, thi
  * 
  * Description of structure of meta node
  * +-------------------------------------------------------------------------------------------------------------------------------------+
- * |next node|record location(1)|data(1)|next 0 if none(1)|prev 0 if none(1)|last fill loc(1)|last free loc(1)|lastFid(1)|end(1)|waste(1)|
+ * |next node|record location(1)|data(1)|file name(8)|next 0 if none(1)|prev 0 if none(1)|last fill loc(1)|last free loc(1)|lastFid(1)|end(1)|waste(1)|
  * +-------------------------------------------------------------------------------------------------------------------------------------+
- * 				 	                   <-stat of meta node
+ * 				 	                              <-stat of meta node
  * 
  * Data can either be the fid in the case of a fillNode or length in the case of a FreeNode
  * Fill nodes are files currently stored
  * Free nodes are deleted files
+ * file name can be 7 characters long as it must be 0 terminated
  * 
  */
 
 /**
  * TODO
- * add support ot append to file
  * add interface
  * get stuff from filesystem
  * take info from importmemory in cpu.c and put in firstRun
+ * add support ot append to file (need to add to freeList)
  */
 
 //run only once, ever. Sets up filesystem parameters
@@ -66,12 +71,13 @@ void fs_init(){
 
 /**
  * Stores data into filesystem
+ * if name==0 then file does not have name
  * First version: Just add to filesystem assuming all previous space used **DONE**
  * Second version: Check to see if contigous space available from previously deleted files, then add to end if not, this requires delete to be implemented
  * Third version: Second version with additional check to see if non-contigous space available before adding to end. this requires delete to be implemented
  * Fourth version: defragmenter. This will need to solve the problem of small fragments smaller then the header size of a record
  */
-void fs_store(int len, int* data){
+void fs_store(int len, int* data, int* name){
   int prev,loc,i;
  
   fs_init();
@@ -82,7 +88,16 @@ void fs_store(int len, int* data){
     filesystem[lastFill]=0;//no prev
     filesystem[lastFill-1]=0;//no next
     filesystem[lastFill-2]=lastFid++;
-    filesystem[lastFill-3]=0;//since no records start at beginning
+    //if name not equal
+    if(name){
+      int i;
+      //copy name
+      for(i=3;i<10&&name[i];i++)
+	filesystem[lastFill-i]=name[i];
+     filesystem[lastFill-i]=0;
+    }else
+      filesystem[lastFill-3]=0;//terminate
+    filesystem[lastFill-11]=0;//since no records start at beginning
     end=lastFill;//last record of meta data
     filesystem[FILESYSTEM_SIZE-2]=end;
     filesystem[FILESYSTEM_SIZE-3]=lastFid;
@@ -96,8 +111,8 @@ void fs_store(int len, int* data){
     filesystem[lastFill-2]=lastFid++;
     
     prev=filesystem[lastFill];
-    loc=filesystem[prev-3];//location of prev data
-    filesystem[lastFill-3]=filesystem[loc+3]+4;//size of data plus 4 (for header info)
+    loc=filesystem[prev-11];//location of prev data
+    filesystem[lastFill-11]=filesystem[loc+3]+4;//size of data plus 4 (for header info)
  
     filesystem[FILESYSTEM_SIZE-2]=end;
     filesystem[FILESYSTEM_SIZE-3]=lastFid;
@@ -122,6 +137,7 @@ void fs_store(int len, int* data){
  * This will add a node to the free list. 
  * 
  * Since a free node is taken from a fill node, 'end' should not change
+ * Note, does not work yet
  */
 void fs_addFree(int index){
  
@@ -146,7 +162,8 @@ void fs_addFree(int index){
 
 /**
  * Removes an record with fid from filesystem
- * Version1: Removes it then makes record of it in LastFree **Done**
+ * Version1: Removes it **Done**
+ * Version2: makes record of it in LastFree
  */
 void fs_delete(int fid){
   //in case not initialized already
@@ -162,21 +179,24 @@ void fs_delete(int fid){
       
       //if lastFill is being removed, set lastFill to prev
 //issue
-      if(lastFill==lastFree)
+      if(lastFill==hasPrev){
 	lastFill=filesystem[lastFill];
+        filesystem[FILESYSTEM_SIZE-5]=lastFill;
+      }else{
 //end issue
-      
-      next=filesystem[hasPrev-1];//get next
-      prev=filesystem[hasPrev];//get prev
-      
-      //if there is a next then give it location of new prev
-      if(next){
-	filesystem[next]=prev;
-      }
-      
-      //if there is a prev then give it location of new next
-      if(prev){
-	filesystem[prev-1]=next;
+
+	next=filesystem[hasPrev-1];//get next
+	prev=filesystem[hasPrev];//get prev
+	
+	//if there is a next then give it location of new prev
+	if(next){
+	  filesystem[next]=prev;
+	}
+	
+	//if there is a prev then give it location of new next
+	if(prev){
+	  filesystem[prev-1]=next;
+	}
       }
       break;
     }
@@ -203,6 +223,131 @@ void fs_list(){
 }
 
 /**
+ * Begin interface items
+ * This struct should be moved to a different file. Possibly filesystem_intf.h.
+ * 
+ * fid: The unique identifier for an inode as used by the filesystem
+ * filename: The name of the file as a string
+ * length: The number of 'words' in the inode, a word is an int in our filesystem
+ * 
+ */
+typedef struct {
+  int fid;
+  char* filename;
+  int length;
+}inode;
+
+/**
+ * Returns a list of filled inodes in the filesystem
+ */
+inode* fs_list_intf(){
+  //in case not initialized already
+  fs_init();
+ 
+  int hasPrev=lastFill;
+  int count=0;
+  int dataLoc=0;
+  int i;
+  
+  while(hasPrev)
+    count++;
+  
+  inode toReturn[count];
+  
+  hasPrev=lastFill;
+  while(hasPrev){
+    count--;
+    toReturn[count].fid=filesystem[hasPrev-2];
+    //copy filename
+      for(i=3;i<10&&filesystem[hasPrev-i];i++)
+	toReturn[count].filename[i]=filesystem[hasPrev-i];
+     toReturn[count].filename[i-3]=0;
+    
+    dataLoc=filesystem[hasPrev-11];//location of data
+    toReturn[count].length=filesystem[dataLoc+3];//length of data
+
+    hasPrev=filesystem[hasPrev];
+  }
+}
+
+/**
+ * IFACE
+ * Returns data based upon a file id. It takes data from the offset to the length, assuming offset+length < the total file length
+ * If offset is greater than file length, it will return 0
+ * If offset+length is greater than file length, but offset is less than file length, it will read to the end of the file
+ * toReturn is a supplied buffer of maximum length 'length'
+ * 
+ * Warning, has not been fully tested
+ */
+void fs_get_intf(int fid, int offset, int length, int* toReturn){
+
+  int hasPrev=lastFill;
+  int dataLoc=0;
+  int dataLen=0;
+  int i;
+  
+  while(hasPrev){
+    //check for match
+    if(filesystem[hasPrev-2]==fid){
+      dataLoc=filesystem[hasPrev-11];      
+      dataLen=filesystem[dataLoc+3];
+
+      //ensure offset is less than file length      
+      if(offset>=dataLen)
+	toReturn=0;
+      
+      //ensure sticks within file
+      if(offset+length>dataLen)
+	length=dataLen-offset;
+
+      //dataLoc location of record
+      //4 is the offset where data portion of record is located
+      //offset is offset from beginning of record to interested data
+      for(i=0;i<length;i++){
+	toReturn[i]=filesystem[dataLoc+4+offset+i];
+      }
+      
+      break;
+    }    
+    hasPrev=filesystem[hasPrev];
+  }
+
+  if(hasPrev)
+    toReturn=0;
+  
+}
+
+/**
+ * Stores new data to the filesystem, then returns the corresponding inode
+ * 
+ */
+inode fs_store_intf(int* data){
+
+  int len=sizeof(data)/sizeof(data[0]);  //determine length of data
+  int* name=0;//no idea how to get this currently, might not use it
+  int dataLoc=0;
+  int i;
+  inode toReturn;
+  
+  //will always be stored as lastFilled.
+  fs_store(len, data, 0);
+  
+  toReturn.fid=filesystem[lastFill-2];
+    //copy filename
+    for(i=3;i<10&&name[i];i++)
+      toReturn.filename[i]=filesystem[lastFill-i];
+    toReturn.filename[i-3]=0;
+    
+    dataLoc=filesystem[lastFill-11];//location of data
+    toReturn.length=len;//length of data  
+ 
+}
+/**
+ * End interface items
+ */
+
+
+/**
  * Tester for the filesystem
  */
 void main(){
@@ -215,10 +360,10 @@ void main(){
   int d[4]={1,2,3,4};
   
   fs_firstRun();
-  fs_store(1,a);
-  fs_store(2,b);
-  fs_store(3,c);
-  fs_store(4,d);
+  fs_store(1,a,0);
+  fs_store(2,b,0);
+  fs_store(3,c,0);
+  fs_store(4,d,0);
   printf("Input test\n");
   fs_list();
   fs_delete(2);
