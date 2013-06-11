1ba5b0c9bf8102876e3f3b0d3851505528f5e361
Eric Nielsen <neric@eagles.ewu.edu>
2013-06-10 18:05:59 -0700

    Added printf's that Kosuke sent an email about

--- source.cpu/cpu.c
+++ source.cpu/cpu.c
@@ -41,7 +41,6 @@ executeit() {
 cont:
     if(locked == UNLOCKED)
 	   cur_proc = scheduler_nextProcess(pic);
-      //cur_proc = (pid == 1) ? 0:(rand()%(pid - 1)) + 1;
 
     if(proc_complete[cur_proc] == 1) {
       printf("----------------------------cur_proc: %d\n", cur_proc);
@@ -130,6 +129,9 @@ int exe(int stack[][STACKSIZE], int sp[], int reg[][REGISTERSIZE], int next_inst
  	  if(wait_state[cur_proc] == READY || wait_time[cur_proc] == READY) {
 		wait_time[cur_proc] = 100;
 		wait_state[cur_proc] = WAITING;
+		heavyLine();
+		printf("Process %d is entering waiting state\n", cur_proc);
+		heavyLine();
 	  }
       else
       tmp = peek(stack,cur_proc,sp, 0);
@@ -149,6 +151,9 @@ int exe(int stack[][STACKSIZE], int sp[], int reg[][REGISTERSIZE], int next_inst
 	  if(wait_state[cur_proc] == READY || wait_time[cur_proc] == READY) {
 		wait_time[cur_proc] = 100;
 		wait_state[cur_proc] = WAITING;
+		heavyLine();
+		printf("Process %d is entering waiting state\n", cur_proc);
+		heavyLine();
 	  }
       else
       tmp = peek(stack, cur_proc, sp, 0);
