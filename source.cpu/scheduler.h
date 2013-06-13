f9f4b6817e40af764425cec1d98c82e9ae9e3875
Eric Nielsen <neric@eagles.ewu.edu>
2013-06-13 09:50:08 -0700

    updated scheduler.c and .h

--- source.cpu/scheduler.h
+++ source.cpu/scheduler.h
@@ -1,32 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "config.h"
+#include "filesystem2.h"
 
-#define READY -1
-#define WAITING 1
-#define DONE_WAITING 2
-#define MAXPRO 6
-
-int wait_state[MAXPRO];
-int wait_time[MAXPRO];
-
-extern int scheduler_nextProcess(int pid);
-extern void scheduler_init(int pid);
-
-
-
-
-
-
-
-
-      
-      
-      
-
-      
-      
-      
-      
-      
-      
-
+void scheduler_init();
+int scheduler_nextProcess(int pid);
